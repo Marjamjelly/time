ad7968de40adce048d0f5ff8f95713ef76739165
Marjamjelly <130711938+Marjamjelly@users.noreply.github.com>
2025-08-11 19:09:17 -0500

    add a time for the increment

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const timer = document.getElementById("timer");
 setInterval(
     function () {
         timer = timer++
-    },
+    }, 1000
 );
 
 document.getElementById("timer").textContent = timer
